a3434997b017dd028ba8828bc8949797abd92679
BILAL HALILOGLU <bhaliloglu@tci.aero>
2026-05-04 14:46:02 +0300

    Articles View Model part 2

--- shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesViewModel.kt
@@ -1,13 +1,26 @@
 package com.bilal.dailypluse.articles
 
 import com.bilal.dailypluse.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ArticlesViewModel : BaseViewModel() {
 
     private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
 
     val articlesState: StateFlow<ArticlesState> = _articlesState
+
+    init {
+        getArticles()
+    }
+
+    private fun getArticles() {
+        scope.launch {
+            delay(500)
+            _articlesState.emit(ArticlesState())
+        }
+    }
 
 }
