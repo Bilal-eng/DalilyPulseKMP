e38ab32f2b55365c8d3aaf67bb2f3143875fda7e
BILAL HALILOGLU <bhaliloglu@tci.aero>
2026-05-07 07:56:42 +0300

    Parsing the articles response

---
+++ shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesResponse.kt
@@ -0,0 +1,17 @@
+package com.bilal.dailypluse.articles
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ArticlesResponse(
+
+    @SerialName("status")
+    val status: String,
+
+    @SerialName("totalResults")
+    val results: Int,
+
+    @SerialName("articles")
+    val articles: List<ArticleRaw>
+)
