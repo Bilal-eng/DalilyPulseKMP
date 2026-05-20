3fa3ee6d8e8f23196b7ba10af15bf0f742c39e4c
BILAL HALILOGLU <bhaliloglu@tci.aero>
2026-05-20 11:19:59 +0300

    Connecting all the pieces

--- shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesUseCase.kt
@@ -1,22 +1,23 @@
 package com.bilal.dailypluse.articles
 
+private const val DEFAULT_IMAGE =
+    "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
+
 class ArticlesUseCase(private val service: ArticlesService) {
 
-    suspend fun getArticles(): List<Article> {
-
-        val articlesRaw = service.fetchArticles()
-
-        return mapArticles(articlesRaw)
-    }
-
-    private fun mapArticles(articlesRaw: List<ArticleRaw>): List<Article> =
-        articlesRaw.map { articleRaw ->
-            Article(
-                articleRaw.title,
-                articleRaw.desc ?: "Click to find out more",
-                articleRaw.date,
-                articleRaw.imageUrl
-                    ?: "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
-            )
+    suspend operator fun invoke(): List<Article> =
+        service.fetchArticles().map {
+            it.toArticle()
         }
+
+
+    private fun ArticleRaw.toArticle(): Article {
+        return Article(
+            title = title,
+            desc = desc ?: "Click to find out more",
+            date = date,
+            imageUrl = imageUrl
+                ?: DEFAULT_IMAGE
+        )
+    }
 }
