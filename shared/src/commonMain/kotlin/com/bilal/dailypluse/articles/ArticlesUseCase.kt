6931539d86aa4c0ec4c9f0406efb2bf5fb5bd20b
BILAL HALILOGLU <bhaliloglu@tci.aero>
2026-05-20 15:30:25 +0300

    Formatting the date

--- shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/bilal/dailypluse/articles/ArticlesUseCase.kt
@@ -1,5 +1,13 @@
 package com.bilal.dailypluse.articles
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+
 private const val DEFAULT_IMAGE =
     "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
 
@@ -15,9 +23,22 @@ class ArticlesUseCase(private val service: ArticlesService) {
         return Article(
             title = title,
             desc = desc ?: "Click to find out more",
-            date = date,
+            date = getDaysAgoString(date),
             imageUrl = imageUrl
                 ?: DEFAULT_IMAGE
         )
+    }
+
+    private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        return when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
     }
 }
