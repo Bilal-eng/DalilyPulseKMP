b61dc332672d43ce6af1f0ce6c4b453d0fc4a835
Bilal-eng <bilal.haliloglu90@gmail.com>
2026-05-21 15:02:32 +0300

    Add IOS navigation

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,12 +1,28 @@
-import SwiftUI
 import Shared
-
+import SwiftUI
 
 struct ContentView: View {
+
+    @State private var shouldOpenAbout = false
+
     var body: some View {
-        ArticlesScreen(
-            viewModel: .init()
-        )
+        NavigationStack {
+            ArticlesScreen(viewModel: .init())
+
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        shouldOpenAbout = true
+                    } label: {
+                        Label("About", systemImage: "info.circle")
+                            .labelStyle(.titleAndIcon)
+                    }
+                    .popover(isPresented: $shouldOpenAbout) {
+                        AboutScreen()
+                    }
+                }
+            }
+        }
     }
 }
 
