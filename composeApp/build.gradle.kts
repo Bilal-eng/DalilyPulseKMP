8383cbef549e9fa17999614fb24ab895d8d04c16
Bilal-eng <bilal.haliloglu90@gmail.com>
2026-05-22 12:54:36 +0300

    Integrating Koin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,5 +69,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.compose.material.icons)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
 }
 
